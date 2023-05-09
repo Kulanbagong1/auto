4f2b9bdbec7580f6d8060abfc6bc91b0b3f0ae39
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-05-10 00:42:20 +0700

    Create addsstp.sh

---
+++ sstp/addsstp.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Wa  : +6283160045408
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+clear
+source /var/lib/wisnucs/ipvps.conf
+if [[ "$IP2" = "" ]]; then
+domain=$(cat /etc/xray/domain)
+else
+domain=$IP2
+fi
+IP=$(wget -qO- ipinfo.io/ip);
+sstp="$(cat ~/log-install.txt | grep -i SSTP | cut -d: -f2|sed 's/ //g')"
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+		read -rp "Usernew : " -e user
+		CLIENT_EXISTS=$(grep -w $user /var/lib/wisnucs/data-user-sstp | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+			echo ""
+			echo -e "Username ${RED}${user}${NC} Already On VPS Please Choose Another"
+			exit 1
+		fi
+	done
+read -p "Password : " pass
+read -p "Expired (Days) : " masaaktif
+hariini=`date -d "0 days" +"%Y-%m-%d"`
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+cat >> /home/sstp/sstp_account <<EOF
+$user * $pass *
+EOF
+
+echo -e "### $user $exp >> /var/lib/wisnucs/data-user-sstp"
+clear
+cat <<EOF
+
+echo -e ""
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[46;1;46m         🔰 AKUN SSTP 🔰          \E[0m"   
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+echo -e "🔺️IP ➡️ $IP"
+echo -e "🔺️Domain ➡️ $domain"
+echo -e "🔺️Username ➡️ $user"
+echo -e "🔺️Password ➡️ $pass"
+echo -e "🔺️Port ➡️ $sstp"
+echo -e "🔺️Cert ➡️ http://$IP:88/server.crt"
+echo -e "🔺️Dibuat ➡️ $hariini"
+echo -e "🔺️Kadaluarsa ➡️ $exp"
+echo -e ""
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[46;1;46m   🔰LUXURY EDITION ZEROSSL🔰     \E[0m"   
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+read -n 1 -s -r -p "Ketik Bebas Untuk Ke Menu Utama"
+menu
